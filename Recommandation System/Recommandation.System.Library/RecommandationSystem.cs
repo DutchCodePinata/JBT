ece2d37789c30eeaaec2ebf80613d7558a7ee3fd
thijs+github@vredenbregt.net <wel5linGH>
2014-05-13 22:56:50 +0200

    update

--- Recommandation System/Recommandation.System.Library/RecommandationSystem.cs
+++ Recommandation System/Recommandation.System.Library/RecommandationSystem.cs
@@ -7,67 +7,12 @@ namespace Recommandation.System.Library
 {
     public class RecommandationSystem
     {
-        private Dictionary<int, Preference> users = new Dictionary<int, Preference>();
-        private List<int> productID = new List<int>();
-        private string localpath = "c:\\Users\\Gameblade\\Desktop\\testdata.txt";
+        private double[][] rawData;
 
         public RecommandationSystem()
         {
-            getDataSet();
-            createAllPearson();    
-        }
-
-        public void getDataSet()
-        {
-//            CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
-//            ci.NumberFormat.NumberDecimalSeparator = ".";
-//            StreamReader myFile = new StreamReader(localpath);
-//            string userString;
-//            while ((userString = myFile.ReadLine()) != null)
-//            {
-//                string[] userinfo = userString.Split(',');
-//                if (!users.ContainsKey(int.Parse(userinfo[0])))
-//                {
-//                    users.Add(int.Parse(userinfo[0]), new Preference(int.Parse(userinfo[0])));
-//                }
-//                if (!productID.Contains(int.Parse(userinfo[1])))
-//                {
-//                    productID.Add(int.Parse(userinfo[1]));
-//                }
-//                Preference pref = users[int.Parse(userinfo[0])];
-//                pref.Add(int.Parse(userinfo[1]), double.Parse(userinfo[2], ci));
- //           }
-
-//            myFile.Close();
-
-            //Display the file contents.
-//            Console.WriteLine("Userdata loaded");
-            //Suspend the screen.
-//            Console.ReadLine();
             Console.WriteLine("\nBegin k-means clustering demo\n");
-
-            // real data likely to come from a text file or SQL
-            double[][] rawData = new double[20][];
-            rawData[0] = new double[] { 65.0, 220.0 };
-            rawData[1] = new double[] { 73.0, 160.0 };
-            rawData[2] = new double[] { 59.0, 110.0 };
-            rawData[3] = new double[] { 61.0, 120.0 };
-            rawData[4] = new double[] { 75.0, 150.0 };
-            rawData[5] = new double[] { 67.0, 240.0 };
-            rawData[6] = new double[] { 68.0, 230.0 };
-            rawData[7] = new double[] { 70.0, 220.0 };
-            rawData[8] = new double[] { 62.0, 130.0 };
-            rawData[9] = new double[] { 66.0, 210.0 };
-            rawData[10] = new double[] { 77.0, 190.0 };
-            rawData[11] = new double[] { 75.0, 180.0 };
-            rawData[12] = new double[] { 74.0, 170.0 };
-            rawData[13] = new double[] { 70.0, 210.0 };
-            rawData[14] = new double[] { 61.0, 110.0 };
-            rawData[15] = new double[] { 58.0, 100.0 };
-            rawData[16] = new double[] { 66.0, 230.0 };
-            rawData[17] = new double[] { 59.0, 120.0 };
-            rawData[18] = new double[] { 68.0, 210.0 };
-            rawData[19] = new double[] { 61.0, 130.0 };
+            getDataSet();
 
             Console.WriteLine("Raw unclustered data:\n");
             Console.WriteLine("    Height Weight");
@@ -89,26 +34,41 @@ namespace Recommandation.System.Library
 
             Console.WriteLine("\nEnd k-means clustering demo\n");
             Console.ReadLine();
+            
         }
 
-        public int[] Cluster(double[][] rawData, int numClusters)
+        public void getDataSet()
         {
-            // k-means clustering
-            // index of return is tuple ID, cell is cluster ID
-            // ex: [2 1 0 0 2 2] means tuple 0 is cluster 2, tuple 1 is cluster 1, tuple 2 is cluster 0, tuple 3 is cluster 0, etc.
-            // an alternative clustering DS to save space is to use the .NET BitArray class
-            double[][] data = Normalized(rawData); // so large values don't dominate
+            rawData = new double[20][];
+            rawData[0] = new double[] { 65.0, 220.0 };
+            rawData[1] = new double[] { 73.0, 160.0 };
+            rawData[2] = new double[] { 59.0, 110.0 };
+            rawData[3] = new double[] { 61.0, 120.0 };
+            rawData[4] = new double[] { 75.0, 150.0 };
+            rawData[5] = new double[] { 67.0, 240.0 };
+            rawData[6] = new double[] { 68.0, 230.0 };
+            rawData[7] = new double[] { 70.0, 220.0 };
+            rawData[8] = new double[] { 62.0, 130.0 };
+            rawData[9] = new double[] { 66.0, 210.0 };
+            rawData[10] = new double[] { 77.0, 190.0 };
+            rawData[11] = new double[] { 75.0, 180.0 };
+            rawData[12] = new double[] { 74.0, 170.0 };
+            rawData[13] = new double[] { 70.0, 210.0 };
+            rawData[14] = new double[] { 61.0, 110.0 };
+            rawData[15] = new double[] { 58.0, 100.0 };
+            rawData[16] = new double[] { 66.0, 230.0 };
+            rawData[17] = new double[] { 59.0, 120.0 };
+            rawData[18] = new double[] { 68.0, 210.0 };
+            rawData[19] = new double[] { 61.0, 130.0 };
 
+            
+        }
+
+        public int[] Cluster(double[][] data, int numClusters)
+        {
             bool changed = true; // was there a change in at least one cluster assignment?
             bool success = true; // were all means able to be computed? (no zero-count clusters)
 
-            // init clustering[] to get things started
-            // an alternative is to initialize means to randomly selected tuples
-            // then the processing loop is
-            // loop
-            //    update clustering
-            //    update means
-            // end loop
             int[] clustering = InitClustering(data.Length, numClusters, 0); // semi-random initialization
             double[][] means = Allocate(numClusters, data[0].Length); // small convenience
 
@@ -117,48 +77,11 @@ namespace Recommandation.System.Library
             while (changed == true && success == true && ct < maxCount)
             {
                 ++ct; // k-means typically converges very quickly
-                success = UpdateMeans(data, clustering, means); // compute new cluster means if possible. no effect if fail
-                changed = UpdateClustering(data, clustering, means); // (re)assign tuples to clusters. no effect if fail
+                success = UpdateMeans(data, clustering, means); 
+                changed = UpdateClustering(data, clustering, means);
             }
-            // consider adding means[][] as an out parameter - the final means could be computed
-            // the final means are useful in some scenarios (e.g., discretization and RBF centroids)
-            // and even though you can compute final means from final clustering, in some cases it
-            // makes sense to return the means (at the expense of some method signature uglinesss)
-            //
-            // another alternative is to return, as an out parameter, some measure of cluster goodness
-            // such as the average distance between cluster means, or the average distance between tuples in 
-            // a cluster, or a weighted combination of both
+            
             return clustering;
-        }
-
-        private double[][] Normalized(double[][] rawData)
-        {
-            // normalize raw data by computing (x - mean) / stddev
-            // primary alternative is min-max:
-            // v' = (v - min) / (max - min)
-
-            // make a copy of input data
-            double[][] result = new double[rawData.Length][];
-            for (int i = 0; i < rawData.Length; ++i)
-            {
-                result[i] = new double[rawData[i].Length];
-                Array.Copy(rawData[i], result[i], rawData[i].Length);
-            }
-
-            for (int j = 0; j < result[0].Length; ++j) // each col
-            {
-                double colSum = 0.0;
-                for (int i = 0; i < result.Length; ++i)
-                    colSum += result[i][j];
-                double mean = colSum / result.Length;
-                double sum = 0.0;
-                for (int i = 0; i < result.Length; ++i)
-                    sum += (result[i][j] - mean) * (result[i][j] - mean);
-                double sd = sum / result.Length;
-                for (int i = 0; i < result.Length; ++i)
-                    result[i][j] = (result[i][j] - mean) / sd;
-            }
-            return result;
         }
 
         private int[] InitClustering(int numTuples, int numClusters, int randomSeed)
@@ -343,70 +266,6 @@ namespace Recommandation.System.Library
                 }
                 Console.WriteLine("===================");
             } // k
-        }
-
-        public void createAllPearson()
-        {
-            foreach (KeyValuePair<int, Preference> pref in users)
-            {
-                createPearson(pref.Value);
-            }
-            Console.ReadLine();
-        }
-
-        public void createPearson(Preference user)
-        {
-            double teller;
-            double noemerA;
-            double noemerB;
-            double pearson;
-            double fase1;
-            double fase2;
-            foreach (KeyValuePair<int, Preference> pref in users)
-            {
-                teller = 0;
-                noemerA = 0;
-                noemerB = 0;
-
-                if (user.getID() != pref.Key)
-                {
-                    foreach (int ID in user.getItem())
-                    {
-                        if (Array.BinarySearch(pref.Value.getItem(), ID) >= 0)
-                        {
-                            fase1 = (user.getRating()[Array.BinarySearch(user.getItem(), ID)] - user.getAverageRating());
-                            fase2 = (pref.Value.getRating()[Array.BinarySearch(pref.Value.getItem(), ID)] - pref.Value.getAverageRating());
-                            teller += fase1 * fase2;
-                            noemerA += Math.Pow(user.getRating()[Array.BinarySearch(user.getItem(), ID)] - user.getAverageRating(), 2);
-                            noemerB += Math.Pow(pref.Value.getRating()[Array.BinarySearch(pref.Value.getItem(), ID)] - pref.Value.getAverageRating(), 2);
-                        }
-                    }
-                    pearson = teller / Math.Sqrt(noemerA * noemerB);
-                    //Display the file contents.
-                    //Console.WriteLine(teller + " / " + Math.Sqrt(noemerA * noemerB));
-                    Console.WriteLine(user.getID() + " -> " + pref.Value.getID() + " | " + pearson);
-                    //Suspend the screen.
-                }
-            }
-            Console.WriteLine();
-        }
-
-
-        public void getInfo(Preference user)
-        {
-            //Console.WriteLine(user.getID());
-
-            int userid = user.getID();
-            int[] items = user.getItem();
-            double[] ratings = user.getRating();
-            int teller = 0;
-            foreach (int i in items)
-            {
-                Console.WriteLine(userid + " " + items[teller] + " " + ratings[teller]);
-                teller++;
-            }
-
-            Console.ReadLine();
         }
     }
 }
